2572a14b01a94579478100adc762791687d47f20
liamf9 <liam@lfitz.net>
2016-06-11 17:08:13 -0700

    style adjustments

--- bst_functions.h
+++ bst_functions.h
@@ -4,19 +4,17 @@ struct Leaf {
 	int value;
 };
 
-void insert( Leaf *&root, int val )
+void insert(Leaf *&root, int val)
 {
 	Leaf *current, *parent;
-	if( root == NULL )
-	{
+	if( root == NULL ) {
 		root = new Leaf;
 		root->value = val;
 		root->left = root->right = NULL;
 		current = root;
 	} else {
 		current = root;
-		while(current!=NULL)
-		{
+		while( current != NULL ) {
 			if( val < current->value ) {
 				parent = current;
 				current = current->left;
@@ -25,16 +23,17 @@ void insert( Leaf *&root, int val )
 				current = current->right;
 			}
 		}
-		Leaf *node = new Leaf;
-		node->value = val;
-		node->left = node->right = NULL;
 
-		if(val < parent->value) parent->left = node;
-		else parent->right = node;
+		Leaf *leaf = new Leaf;
+		leaf->value = val;
+		leaf->left = leaf->right = NULL;
+
+		if( val < parent->value ) parent->left = leaf;
+		else parent->right = leaf;
 	}
 }
 
-void inorder(Leaf* &root)
+void inorder(Leaf *&root)
 {
 	std::stack<Leaf*> s;
 	while(!s.empty() || root != NULL)
